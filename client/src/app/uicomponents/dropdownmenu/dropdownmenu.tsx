a1223d035a80db7723aa7c0885fbb02e90c4ad3b
Yekeen Jimoh <yekeenjimoh04@gmail.com>
2024-05-28 23:38:47 +0100

    made ts changes

--- client/src/app/uicomponents/dropdownmenu/dropdownmenu.tsx
+++ client/src/app/uicomponents/dropdownmenu/dropdownmenu.tsx
@@ -1,17 +1,18 @@
-import { DropdownContext } from '@/app/contexts/timeRangeContext';
-import Merchant from '@/app/merchants/page';
 import React, { useState, useContext, useEffect } from 'react';
 import styles from './dropdown.module.css'
 
 // Step 1: Create a context to share the dropdown state
 
+interface DropdownMenuProps {
+  sendTimeRange: (timeRange: DropdownOptionType) => void;
+}
 
 
-const DropdownMenu = ({sendTimeRange})=> {
+const DropdownMenu: React.FC<DropdownMenuProps> = ({sendTimeRange})=> {
 
   const [timeRange, setTimeRange] =  useState<DropdownOptionType>('month')
-  const handleSelectChange = (option: DropdownOptionType) => {
-    setTimeRange(option);
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setTimeRange(event.target.value as DropdownOptionType);
   };
   useEffect(() =>{
     sendTimeRange(timeRange)
@@ -23,7 +24,7 @@ const DropdownMenu = ({sendTimeRange})=> {
           className={styles.select}
           id="dropdown"
           value={timeRange}
-          onChange={(e) => handleSelectChange(e.target.value)}
+          onChange={(e) => handleSelectChange}
         >
           <option value = 'month'>Month</option>
           <option value="7days">7 Days</option>
