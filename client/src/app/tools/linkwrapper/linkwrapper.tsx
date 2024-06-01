7d3307443bab3f2a77ae1e12cfdc5c4f6c800792
Yekeen Jimoh <yekeenjimoh04@gmail.com>
2024-06-01 20:37:17 +0100

    added url shortner

--- client/src/app/tools/linkwrapper/linkwrapper.tsx
+++ client/src/app/tools/linkwrapper/linkwrapper.tsx
@@ -24,6 +24,25 @@ export default function Linkwrapper() {
   const [copied, setCopied] = useState(false)
   const [InputValue, setInputValue] = useState('')
   const [username, setUsername] = useState('');
+  const [urlShortened,setUrlShortened] =useState(true)
+
+  const shortenUrl = async (xcust: string) =>{
+    try{
+      const response = await fetch(`https://buyth.at/-make?id=225413X1707292&url=${userText}&xcust=${xcust}`)
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json()
+      console.log(data)
+      if(data.status == 0){
+        return data['shorturl']
+      }
+      throw Error
+    }catch(err){
+      console.log(err)
+      return 'could not shortenurl'
+    }
+  }
 
 
   useEffect(() => {
@@ -47,12 +66,22 @@ export default function Linkwrapper() {
       setUsername(decryptedUsername);
     }
   }, []);
+  const shortenUrlHandler = async() =>{
+    const resFromUrlShortner = await shortenUrl(username)
+    setAffiliatedLink(resFromUrlShortner)
+    setUrlShortened(true)
+  }
 
+  const pasteHandler = (event: React.ClipboardEvent<HTMLInputElement>): void => {
+    const pastedText = event.clipboardData.getData('text');
+    setInputValue(pastedText); // Update the state with the pasted text
+  }
   const generateLink = () =>{
     if(!userText){
       setAffiliatedLink('Please enter a link')
     }else{
       setAffiliatedLink(`https://go.skimresources.com/?id=225413X1707292&url=${userText}&xcust=${username}`)
+      setUrlShortened(false)
     }
   }
   const copyToClipboard  = async() =>{
@@ -70,13 +99,14 @@ export default function Linkwrapper() {
       <div className={styles.container}>
         <div className= {styles.firstsection}>
           <div>
-            <input className = {styles.inputbox} placeholder='Enter link here' onChange={(e)=>{setInputValue(e.target.value); setUserText(InputValue) ; setCopied(false)}} value={InputValue}></input>
+            <input className = {styles.inputbox} placeholder='Enter link here' onChange={(e)=>{setInputValue(e.target.value); setUserText(InputValue) ; setCopied(false)}} onPaste={pasteHandler} value={InputValue}></input>
             <button className= {styles.button} onClick={() =>{ setInputValue(''); setCopied(false); setAffiliatedLink('')}}> Clear</button>
           </div>
           <button className = {styles.button} onClick={generateLink}>Generate Link</button>
         </div>
         <textarea className = {styles.textarea} defaultValue={affiliatedLink}></textarea>
-        <button className = {styles.button} onClick = {copyToClipboard}> { copied ? 'Copied to Clipboard' : 'Copy'}</button>
+        <button className = {styles.button} onClick = {copyToClipboard} > { copied ? 'Copied to Clipboard' : 'Copy'}</button>
+        <button className= {styles.button} onClick = {shortenUrlHandler} disabled ={urlShortened}> Shorten</button>
       </div>
     </div>
   )
