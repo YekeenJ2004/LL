7282544728f85d5dca9db5402d067d0c30290a99
Yekeen Jimoh <yekeenjimoh04@gmail.com>
2024-06-27 22:02:33 +0100

    fixed redirects

--- client/src/app/resetpassword/page.tsx
+++ client/src/app/resetpassword/page.tsx
@@ -14,54 +14,53 @@ export default function ResetPassword() {
     const handleSubmit = async() =>{
 
     }
-    if(){
-        return (
-            <div>
-                <form className={styles.form}>
-                <div className={styles.title}>
-                    Login
-                </div>
-                <div>
-                    <div className={styles.field}>
-                        <input className={styles.inputbox}
-                            type ='text'
-                            value = {otp}
-                            placeholder= 'E-mail'
-                            onChange={(e) => setOtp(e.target.value)}
-                        >
-                        </input>
-                    </div>
-                    <div className={styles.field}>
-                        <input className={styles.inputbox}
-                            type='password'
-                            value = {password}
-                            placeholder= 'New password'
-                            onChange={(e) => setPassword(e.target.value)}
-                        >
-                        </input>
-                    </div>
-                    <div className={styles.field}>
-                        <input className={styles.inputbox}
-                            type='password'
-                            value = {password}
-                            placeholder= 'Confirm new password'
-                            onChange={(e) => setPassword(e.target.value)}
-                        >
-                        </input>
-                        {!isValid && <span className={styles.errormessage}>incorrect OTP email combo</span>}
-                    </div>
-                </div>
-                <div className={styles.buttombuttons}>
-                    <button type = "submit" className={styles.button} onClick={handleSubmit}>Login</button>
-                    <Link href='https://ll-client.vercel.app/apply'>
-                    <button className={styles.button}>Apply now</button>
-                    </Link>
-                </div>
-                <div>
-                    <Link href = 'https://ll-client.vercel.app/resetpassword'>forgot password?</Link>
-                </div>
-                </form>
-            </div>
-        )
-    }
+    return (
+        <div></div>
+        // <div>
+        //     <form className={styles.form}>
+        //     <div className={styles.title}>
+        //         Login
+        //     </div>
+        //     <div>
+        //         <div className={styles.field}>
+        //             <input className={styles.inputbox}
+        //                 type ='text'
+        //                 value = {otp}
+        //                 placeholder= 'E-mail'
+        //                 onChange={(e) => setOtp(e.target.value)}
+        //             >
+        //             </input>
+        //         </div>
+        //         <div className={styles.field}>
+        //             <input className={styles.inputbox}
+        //                 type='password'
+        //                 value = {password}
+        //                 placeholder= 'New password'
+        //                 onChange={(e) => setPassword(e.target.value)}
+        //             >
+        //             </input>
+        //         </div>
+        //         <div className={styles.field}>
+        //             <input className={styles.inputbox}
+        //                 type='password'
+        //                 value = {password}
+        //                 placeholder= 'Confirm new password'
+        //                 onChange={(e) => setPassword(e.target.value)}
+        //             >
+        //             </input>
+        //             {!isValid && <span className={styles.errormessage}>incorrect OTP email combo</span>}
+        //         </div>
+        //     </div>
+        //     <div className={styles.buttombuttons}>
+        //         <button type = "submit" className={styles.button} onClick={handleSubmit}>Login</button>
+        //         <Link href='https://linkloop.app/apply'>
+        //         <button className={styles.button}>Apply now</button>
+        //         </Link>
+        //     </div>
+        //     <div>
+        //         <Link href = 'https://linkloop.app/resetpassword'>forgot password?</Link>
+        //     </div>
+        //     </form>
+        // </div>
+    )
 }
