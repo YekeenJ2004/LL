a32edc3d01554371ccedacd5164e7d3b02902b3a
Yekeen Jimoh <yekeenjimoh04@gmail.com>
2024-07-17 14:40:35 +0100

    reset password

--- server/sendEmail.js
+++ server/sendEmail.js
@@ -1,5 +1,6 @@
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
+import { TEMPORARY_REDIRECT_STATUS } from 'next/dist/shared/lib/constants';
 
 dotenv.config(); // Load environment variables from .env file
 
@@ -25,8 +26,10 @@ export async function sendEmail(to, subject, html) {
     const info = await transporter.sendMail(mailOptions);
     console.log('Message sent: %s', info.messageId);
     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    return true
   } catch (error) {
     console.error('Error sending email:', error);
-    console.error('Error sending email:', error);
+    return false
   }
+
 }
